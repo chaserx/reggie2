756dbdeb3ab6bf44a6b1570337841a00c0320db1
Chase Southard <chase.southard@gmail.com>
2011-02-04 12:24:21 -0500

    initial commit. empty rails 3 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Reggie2::Application.config.secret_token = 'effd43365e352a237554e42a6d73912466ca4ea634d720bb62d5896dd6f1b85d99c71db01dfd035ad039b95d3ceda69f385e25351d60d4b1cfb43a342784e272'
