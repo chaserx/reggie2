a2ee8e2037bc7afb883f8ac5fff43bfeac3aa409
Chase Southard <chase.southard@gmail.com>
2011-03-04 21:33:45 -0500

    Revert "installing devise"
    This reverts commit 4ded19d0526a46575964e0ecf2b7c92e0fc65d3d.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Reggie2::Application.routes.draw do
-  devise_for :users
-  
-  resources :registrations
-
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
