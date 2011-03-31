ef3ba995dd26da729a83a69ca1a2f35ae8a1aa94
Chase Southard <chase.southard@gmail.com>
2011-03-31 17:57:55 -0400

    adding an if statement on User.count to require logged_in before creating a new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
+  if User.count > 0
+    before_filter :logged_in?
+  else
+    raise "User count is zero. Check User table. Perhaps, run rake db:seed"
+  end
+  
   def new
     @user = User.new
   end
@@ -6,7 +12,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
-      redirect_to root_url, :notice => "Signed up!"
+      redirect_to registrations_url, :notice => "Signed up!"
     else
       render "new"
     end
